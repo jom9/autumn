fd88947c9e90887e1b9daf66b3c7df6f6985732c
Jonathan <martinezjonathan4287@gmail.com>
2019-12-30 04:03:29 +0000

    Delete Permanent.py

--- Permanent.py
+++
@@ -1,9 +0,0 @@
-
-class Permanent(Card):
-    def __init__(self,cdname,cdtext,mncost,clr):
-        super().__init__(cdname,cdtext,mncost,clr)
-        self.tapped  = False
-    def tap(self):
-        self.tapped = True
-    def untap(self):
-        self.tapped = False
