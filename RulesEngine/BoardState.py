53c8ace01a9cfc9cb1ae0624a4951919b2b63054
Jonathan <martinezjonathan4287@gmail.com>
2020-01-06 17:57:12 -0500

    Implemented phases

--- RulesEngine/BoardState.py
+++ RulesEngine/BoardState.py
@@ -10,7 +10,9 @@ maxHandSize =7
 class BoardState():
 
     def __init__(self):
-        self.manaPool = [0,0,0,0,0,0]# mana currently in the mana pool in wubrg wingding order
+        self.startmanaPool = [0,0,0,0,0,0]
+        self.manaPool = self.startmanaPool# mana currently in the mana pool in wubrg wingding order
+
         self.stormCount = 0 #number of spells played this turn
         self.playersHand = Hand()
         self.opponentsHand = Hand()
@@ -18,33 +20,69 @@ class BoardState():
         self.opponentsDeck = Deck()
         self.cardsInDeck = defaultDeckSize
 
+        self.playersGraveyard =[]
+        self.opponentsGraveyard = []
+
+        self.playersExile = []
+        self.opponentsExile = []
+
         self.playerLifeTotal = defaultLifeTotal
-        self.opLifeTotal = defaultLifeTotal
+        self.opponetsLifeTotal = defaultLifeTotal
+
         self.stack= Stack()
         self.turnNum = 0
 
         self.playersLands = []
         self.playersCreatures = []
 
-        self.opLands = []
-        self.opCreatures = []
-
-
+        self.opponentsLands = []
+        self.opponentsCreatures = []
+        self.turns = ['player','op']
+        self.phases = [ 'Untap','Upkeep','Draw','PreMain','Begin Combat','Declare Attackers','Declare Blockers',
+        'Combat Damage','End of Combat','Post Combat','End','Cleanup']
+        self.currentPhase = phases[0]
+        self.priority = ''
     def setDecks(self,filename):
         self.playersDeck.getDeckList(filename)
         for i in range(defaultDeckSize):
             self.opponentsDeck.mainboard.append( (Card(),-1) ) # the -1 is denote the number of copies is unknown
 
-    def startMatch(self):
+    def startMatch(self,startingPlayers):
+        self.turns = startingPlayers
+        self.priority = turns
         self.playersDeck.shuffle()
         for i in range(defaultStartHandSize):
 
             self.playersHand.addCard(self.playersDeck.draw())
 
             self.opponentsHand.addCard( self.opponentsDeck.draw() )
-    def takeTurn(self):
-        self.playersHand.addCard( self.playersDeck.draw() )
-        self.turnNum+=1
+    def nextPhase(self):
+        self.manaPool = self.startmanaPool
+        i=-1*len(self.phases)+1
+        for phase in self.phases:
+            if self.currentPhase= phase:
+                break
+            i+=1
+
+        if self.currentPhase == 'Draw':
+            if self.turns == 'player':
+                self.playersHand.addCard(self.playersDeck.draw())
+            elif self.turns == 'op':
+                self.opponentsHand.addCard(self.opponentsDeck.draw())
+        elif self.currentPhase =='Untap':
+            if self.turns == 'player':
+                for land in self.opponentsLands:
+                    land.untap()
+                for creature in self.opponentsCreatures:
+                    creature.untap()
+            elif self.turns == 'op':
+                for land in self.playersLands:
+                    land.untap()
+                for creature in self.playersCreatures:
+                    creature.untap()
+        self.currentPhase = self.phases[i]
+
+
     def showHands(self):
         print('Your Hand:')
         self.playersHand.showHand()
